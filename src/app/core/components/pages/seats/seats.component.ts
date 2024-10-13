f704441f51b92665d265ce6f073634d6efc4cf49
AdrianBa12 <adrianjkba@gmail.com>
2024-10-13 16:17:16 -0500

    Modificacion completa de Seats

--- src/app/core/components/pages/seats/seats.component.ts
+++ src/app/core/components/pages/seats/seats.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderPageComponent } from '../../atoms/header-page/header-page.component';
+import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-seats',
     standalone: true,
-    imports: [HeaderPageComponent],
+    imports: [HeaderPageComponent, CommonModule],
     templateUrl: './seats.component.html',
     styleUrl: './seats.component.scss',
 })
-export class SeatsComponent {}
+export class SeatsComponent {
+    seats = [
+        { number: 1, selected: false },
+        { number: 2, selected: false },
+        { number: 3, selected: false },
+        // Agrega más asientos según sea necesario
+    ];
+
+    selectedSeatsCount = 0;
+
+    toggleSeat(seat: any) {
+        seat.selected = !seat.selected;
+        this.selectedSeatsCount += seat.selected ? 1 : -1;
+    }
+}
